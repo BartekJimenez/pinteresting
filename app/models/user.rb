f83972b6cd2dac20881445c543f4348dc7646854
Zealex <Pwentz9000@yahoo.com>
2016-01-09 20:28:29 -0500

    removed password security

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-#prevent making a bad p/w         
+prevent making a bad p/w         
   validates :password, :presence => true,
              :on => :create,
              :format => {:with => /\A.*(?=.{10,})(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\@\#\$\%\^\&\+\=]).*\Z/ }    
